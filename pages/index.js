db0ecd7769781138781aad4792b3a0f8a67a50be
Rotom da Peste - BR <mielsen.gamer2005@gmail.com>
2022-04-22 02:42:13 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <div>Home</div>
+    return  <div style={{textAlign: "center"}}>
+                <h1>Home</h1>
+                <img src="https://images.gameinfo.io/pokemon/256/p479f58.png"></img>
+            </div>
 }
+
+
 
 export default Home
